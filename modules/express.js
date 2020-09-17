485443428a09f697e020d3c6c3ee890e2e67f54a
Domepo <36802235+Domepo@users.noreply.github.com>
2020-09-17 22:06:48 +0200

    Update express.js

--- modules/express.js
+++ modules/express.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
+var audioControl = require('./audio-control');
 app.listen(8080, ()=>console.log("Server laeuft"));
 app.use(express.static("public"));
 app.use(express.json({limit: '1000mb'}));
 
+const filename = "audio/dadi.mp3" ;
+
+
 app.post("/api",(request, response)=>{
     console.log(request.body);
-    return response.send('Received a POST HTTP method'); 
-    // realy important
-    // if you dont use the respond send function you`ll get a 2 minute delay 
-});
+    if(request.body.name == 1){
+        audioControl.play(filename);
+    }
+    if(request.body.name == 2){
+        audioControl.pause(filename);
+    }
+    return response.send('Received a POST HTTP method');
 
+});
