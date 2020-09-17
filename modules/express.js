15963d81ff9d776fc120ff2d315ffdf7c2eb9f83
Domepo <36802235+Domepo@users.noreply.github.com>
2020-09-17 21:33:57 +0200

    Update express.js

--- modules/express.js
+++ modules/express.js
@@ -6,6 +6,8 @@ app.use(express.json({limit: '1000mb'}));
 
 app.post("/api",(request, response)=>{
     console.log(request.body);
-    return response.send('Received a POST HTTP method');
+    return response.send('Received a POST HTTP method'); 
+    // realy important
+    // if you dont use the respond send function you`ll get a 2 minute delay 
 });
 
