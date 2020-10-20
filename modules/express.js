a1d5950aec2d25024f5cf488798380ca2f7b7c0b
Domepo <36802235+Domepo@users.noreply.github.com>
2020-10-20 19:41:27 +0200

    Update express.js

--- modules/express.js
+++ modules/express.js
@@ -2,16 +2,22 @@ const express = require("express");
 const app = express();
 const fileUpload = require('express-fileupload');
 
+
+var mpg = require('mpg123');
+ 
+var player = new mpg.MpgPlayer();
+
+
+
 var audioControl = require('./audio-control');
 
 app.listen(8080, ()=>console.log("Server laeuft"));
 app.use(express.static("public"));
-app.use(express.json({limit: '1000mb'}));
+app.use(express.json({limit: '10mb'}));
 
 app.use(fileUpload());
-
+const store = require('data-store')({ path: process.cwd() + '/foo.json' });
 const filename = "audio/dadi.mp3" ;
-
 
 app.post("/api",(request, response)=>{
     console.log(request.body);
@@ -21,30 +27,47 @@ app.post("/api",(request, response)=>{
     if(request.body.name == 2){
         audioControl.pause(filename);
     }
-    return response.send('something'); // provide break after 6 inputs
+    return response.send('Return only for purpose lol');
 
 });
 
+// Upload Form
 app.post('/upload', function(req, res) {
-    if (!req.files || Object.keys(req.files).length === 0) {
-      return res.status(400).send('No files were uploaded.');
-    }
-  
-    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-    let sampleFile = req.files.sampleFile;
-  
-    // Use the mv() method to place the file somewhere on your server
-    sampleFile.mv('audio/'+sampleFile.name, function(err) {
-      if (err)
-        return res.status(500).send(err);
-  
-      res.send('File uploaded!');
-    });
-    return true;
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.sampleFile;
+
+  console.log(req.body.sampleLength);
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv('audio/'+sampleFile.name, function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+    // uploadStorage(sampleFile);
+    let test = 'audio/'+sampleFile.name;
+    uploadStorage(sampleFile)
+
+
   });
+});
 
 
+function uploadStorage(file){
+    // Identifier
+    let idForUploadStorage = Object.keys(store.data).length;
+    let FileNameWithoutExtension = file.name.split('.').slice(0, -1).join('.');
+    
+    store.set(String(FileNameWithoutExtension)); 
+    store.set(String(FileNameWithoutExtension)+".time"," String(ou)"); 
+    store.set(String(FileNameWithoutExtension)+".name", String(file.name)); 
+}
 
+// Search in dic
 const path = require('path');
 const fs = require('fs');
 
