e78b559ee08340be2f0e4209b243feda9cb57c67
Domepo <36802235+Domepo@users.noreply.github.com>
2020-10-27 19:29:44 +0100

    Update selector.js

--- public/selector.js
+++ public/selector.js
@@ -1,46 +1,52 @@
 // create the select option menu in HTML
 function createSelectOptionHTML(){
   
-    songProperties()
-      .then(response =>{
-        let createDiv    = document.createElement("div");
-        let createSelect = document.createElement("select");
-  
-        console.log(response.length);
-        response.forEach(element => {
-  
-            let createOption = document.createElement("option");
-  
-            createOption.innerHTML = String(element);
-  
-            createSelect.setAttribute("id","selector");
-            createSelect.setAttribute("onchange","selectListenerPush()");
-  
-            createDiv.setAttribute("name","selectorDiv");
-            createOption.setAttribute("value",String(element));
-  
-            document.body.appendChild(createDiv);
-            createDiv.appendChild(createSelect);
-            createSelect.appendChild(createOption);
-            
-        });         
-      });
-  }
-  
+  songProperties()
+    .then(response =>{
+      let createDiv    = document.createElement("div");
+      let createSelect = document.createElement("select");
+      let createBlankOption = document.createElement("option");
+      createBlankOption.setAttribute("style","display:none;");
+
+      createBlankOption.innerHTML = "Select a song";
+
+      document.body.appendChild(createDiv);
+      createDiv.appendChild(createSelect);
+      createSelect.appendChild(createBlankOption);
+
+      console.log(response.length);
+      response.forEach(element => {
+
+          let createOption = document.createElement("option");
+
+          createOption.innerHTML = String(element);
+
+          createSelect.setAttribute("id","selector");
+          createSelect.setAttribute("onchange","selectListenerPush()");
+
+          createDiv.setAttribute("name","selectorDiv");
+          createOption.setAttribute("value",String(element));
+
+          createSelect.appendChild(createOption);
+          
+      });         
+    });
+}
+
 // Push the menu value via api
 async function selectListenerPush(){
-    let selectorID = document.getElementById("selector");
-    let selectorResult = selectorID.options[selectorID.selectedIndex].value;
-    await fetch("/dir/json/return",{
-            method: "POST",
-            headers:{
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                name: selectorResult
-            })
-        })
-        .catch((err)=>console.log(err));
+  let selectorID = document.getElementById("selector");
+  let selectorResult = selectorID.options[selectorID.selectedIndex].value;
+  await fetch("/dir/json/return",{
+          method: "POST",
+          headers:{
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+              name: selectorResult
+          })
+      })
+      .catch((err)=>console.log(err));
 };
-    
+  
 createSelectOptionHTML();
