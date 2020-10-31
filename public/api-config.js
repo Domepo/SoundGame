e7a8ecc9c411109897f3b63bfff1575ff956ee95
Domepo <36802235+Domepo@users.noreply.github.com>
2020-10-31 12:47:53 +0100

    /audio-controll update for the selector module

--- public/api-config.js
+++ public/api-config.js
@@ -3,54 +3,57 @@
 
 async function AudioControl(state){
     // get the button data for play pause and stop
+    // get the return of selector.js
+    const selectorResult = await selectListenerPush();
     await fetch("/audio-controll",{
             method: "POST",
             headers:{
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                name: state
+                ButtonState: state,
+                SelectedSong: selectorResult
             })
         })
         .catch((err)=>console.log(err));
     };
 
-    async function FilesInDirectory(){
-        // get all uploaded files
-        const response = await fetch("/dir",{
-                method: "POST",
-                headers:{
-                    "Content-Type": "application/json"
-                }
-            })
-        const responseArray = await response.json();
-        const responseFiles = responseArray.files;
-        return responseFiles;
-    };
+async function FilesInDirectory(){
+    // get all uploaded files
+    const response = await fetch("/dir",{
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json"
+            }
+        })
+    const responseArray = await response.json();
+    const responseFiles = responseArray.files;
+    return responseFiles;
+};
 
-    const eachFileInDirectory = FilesInDirectory()
-    // get the latest file in the directoryPath -> ./app.js
-    .then(response =>{
-            let ShowDirectoryElement = document.getElementById("showdirectory");
-            console.log(response.length+" files are Uploaded");
-            ShowDirectoryElement.innerHTML = response[1];
-    });
+const eachFileInDirectory = FilesInDirectory()
+// get the latest file in the directoryPath -> ./app.js
+.then(response =>{
+        let ShowDirectoryElement = document.getElementById("showdirectory");
+        console.log(response.length+" files are Uploaded");
+        ShowDirectoryElement.innerHTML = response[1];
+});
 
 
-    // get data from song-properties.json
-    async function songProperties(){
-        const response = await fetch("/dir/json",{
-                method: "POST",
-                headers:{
-                    "Content-Type": "application/json"
-                }
-            })
-        const responseArray = await response.json();
-        return responseArray;
-    };
+// get data from song-properties.json
+async function songProperties(){
+    const response = await fetch("/dir/json",{
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json"
+            }
+        })
+    const responseArray = await response.json();
+    return responseArray;
+};
 
-    const songPropertiesProm = songProperties()
-    .then(response =>{
-            console.log(response);
-            return response;
-    });
+const songPropertiesProm = songProperties()
+.then(response =>{
+        console.log(response);
+        return response;
+});
