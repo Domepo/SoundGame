226b3c249936089718cb5e42d493f8023f58d620
Domepo <36802235+Domepo@users.noreply.github.com>
2020-10-31 14:23:44 +0100

    added jump command

--- modules/audio-control.js
+++ modules/audio-control.js
@@ -10,5 +10,13 @@ module.exports = {
     },
     pause: function (filename) {
       player.pause(filename);
+    },
+    stop: function (filename) {
+      player.stop(filename);
+    },
+    cmd: function (filename,frames) {
+      player.play(filename);
+      player._cmd("JUMP "+frames);
     }
   };
+// mpg123 -k 2231 alane.mp3
